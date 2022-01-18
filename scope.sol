a5fd388107a4d136369e05ce1ef43f66b366108d
remi <hellokiziche@gmail.com>
2022-01-18 15:07:49 +0100

    external

--- scope.sol
+++ scope.sol
@@ -5,19 +5,21 @@ pragma solidity >= 0.7.0 < 0.9.0;
 
 // Private = You can only call the function inside the contract
 // Internal = called only within the contract OR other contracts that inheit the smart contract (slightly less restrictive than public)
-// External =
+// External = Function can only be called outside of the contract or using "this" if you are inside
 // Public = You can call the function from outside the smart contract as well inside the smart contract
 
 contract Scope {
     uint public data = 10; // state variable
 
-    function x() public view returns(uint){
+    function x() external pure returns(uint){
         //Modify function x so that you return 25 for data wthout changing the data of the state variable
-        uint newData = data + 15;
+        uint newData = 25;
         return newData;
     }
 
     function y() public view returns (uint){
-        return data;
+        return this.x();
+        //Call x() this way -> Internal
+        //Call this.x() -> using this. to call it externaly
     }
 }
