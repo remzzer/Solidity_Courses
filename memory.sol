bc6e20333e2ace90271f3545d8d42d4b54dd6e71
remi <hellokiziche@gmail.com>
2022-01-19 14:31:10 +0100

    convert_to_bytes

--- memory.sol
+++ memory.sol
@@ -19,9 +19,29 @@ contract memoryAndStorage {
     // strings are too expensive computationally to get length
   function getChat() public view returns(uint){
     //return greetings.length; //Output: Error
-    
     //convert strings to bytes:
     bytes memory stringToBytes = bytes(greetings);
     return stringToBytes.length;
   }
+}
+
+
+//1.Create a function that return a color
+//2.Create a function that change color
+//3.Count char from new color (string)
+contract colorString {
+    string favoriteColor = "blue";
+
+    function getColor() public view returns(string memory){
+        return favoriteColor;
+    }
+
+    function changeColor(string memory _color) public returns(string memory){
+        return favoriteColor = _color;
+    }
+
+    function countChar(string memory _str) public pure returns(uint){
+        bytes memory newStr = bytes(_str);
+        return newStr.length;
+    }
 }
