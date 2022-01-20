c10b7b4bdaede3e9962628684010b1049cb369a2
remi <hellokiziche@gmail.com>
2022-01-20 21:41:18 +0100

    arrays

--- arrays.sol
+++ arrays.sol
@@ -5,7 +5,7 @@ pragma solidity >= 0.7.0 < 0.9.0;
 //2. pop, push, and length methods
 //3. remove elements from array
 
-contract learnArrays{
+contract LearnArrays{
     uint[] public myArray;
     uint[] public myArray2;
     uint[200] public myArray3;
@@ -21,4 +21,37 @@ contract learnArrays{
     function getLength() public view returns (uint){
             return myArray.length;
     }
+
+    function remove(uint index) public {
+        delete myArray[index];
+    }
+}
+
+//Exercise
+
+//1. Create an Empty array called changeArray
+//2. Create a function called removeElment which sets the index argument of the array to the last element in the array
+//3. remove the last index from that functionwith the pop methods
+//4. Create a function called test which pushed 1 2 3 4 into changeArray
+//5. remove the element 2 from the array when the contract is called
+
+contract Exercise{
+    uint[] public changeArray;
+    uint i;
+
+    function removeElement(uint _index) public {
+        changeArray[_index] = changeArray[changeArray.length -1];
+        changeArray.pop();
+    }
+
+   function test () public {
+       for (i = 1; i <= 4; i++){
+            changeArray.push(i);
+       }
+   }
+
+   function returnArray() public view returns(uint[] memory){
+       return changeArray;
+   }
+
 }
