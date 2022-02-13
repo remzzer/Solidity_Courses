b5feef51a8329d9b6e4c8976143b629b36cc7e3e
remi <hellokiziche@gmail.com>
2022-02-13 12:22:47 +0100

    modifiers

--- modifiers.sol
+++ modifiers.sol
@@ -3,6 +3,20 @@ pragma solidity >= 0.7.0 < 0.9.0;
 
 contract Owner {
 
+  address owner;
+
+  // when we deploy this contract we want to set the address to the owner (msg.sender)
+  constructor public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner {
+    //customizable logic to modify our functions
+    require (msg.sender == owner);
+    // _ continues with the function
+    _;
+  }
+
 }
 
 contract Register is Owner {
@@ -19,7 +33,10 @@ contract Register is Owner {
     registeredAddresses[msg.sender] = true;
   }
 
+  // onlyOwner is our function modifier that require
+  //only the owner to be able to change the price
   function changePrice(uint _price) public onlyOwner {
     price = _price;
   }
+
 }
