436de3b2d521680816f88ec5493131a84a27f986
remi <hellokiziche@gmail.com>
2022-02-14 19:51:14 +0100

    coins

--- subcurrency.sol
+++ subcurrency.sol
@@ -9,8 +9,33 @@ contract Coin {
     address public minter;
     mapping(address => uint) public balances;
 
+    event Sent(address from, address to, uint amount);
+
     //only runs when we deploy contract
     constructor() {
         minter = msg.sender;
     }
+
+    //make new coins and send them to an address
+    // only the owner can send these coins
+    function mint(address receiver, uint amount) public {
+      require(msg.sender == minter);
+      balances[receiver] += amount;
+    }
+
+    //send any amount of coins to an exising address
+
+    error insufficientBalance(uint requested, uint available);
+
+    function send(address receiver, uint amount) public {
+        //require (amount > balances[msg.sender], "You do not have enough ETH");
+        if (amount > balances[msg.sender])
+        revert insufficientBalance({
+            requested: amount,
+            available: balances[msg.sender]
+        });
+        balances[msg.sender] -= amount;
+        balances[receiver]  += amount;
+    }
+
 }
