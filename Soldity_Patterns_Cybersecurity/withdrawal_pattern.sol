95c1861db18a72cd7f62b82135643db33b9c0eb2
remi <hellokiziche@gmail.com>
2022-02-17 11:05:50 +0100

    secu

--- Soldity_Patterns_Cybersecurity/withdrawal_pattern.sol
+++ Soldity_Patterns_Cybersecurity/withdrawal_pattern.sol
@@ -1,37 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 /*
 Cybersecurity In Solidity
 
-withdrawal pattern ensures that direct transfer call is not made which poses a secuity threat.
-Transfers ae atomic (meaning all or nothing)
+withdrawal pattern ensures that direct transfer call is not made which poses a security threat.
+Transfers are atomic (meaning all or nothing)
 
 Write an iterating function called returnFunds that will refund through the transfer method investors to a var called funders back to their wallets called contributedAmount.
 Add a modifier to the signature that only the owner can execute this function. The function should return a bool success.
+
+Ps: I did the exercise my own way
 */
 
 contract WithdrawPattern  {
 
-    address contributedAmount;
-    mapping(address => uint) balances;
-    uint total;
+    uint contributedAmount;
+    mapping(address => uint) public balances;
 
-    function deposit() external returns (uint){
+    //Mapping that helps retrieve msg.sender and his deposit
+    function deposit() external payable {
         balances[msg.sender] = msg.value;
-        return total += msg.value;
+        contributedAmount += msg.value;
     }
 
-    function returnFunds() public onlyOwner returns (bool){
+    //Total deposit by all owners
+    function getTotal() public view returns (uint){
+        return contributedAmount;
+    }
 
-        /*
-        for(let i = 0; i < funders.length; i++){
-            funders[i].transfer(contributedAmount);
-        }
-        */
-        balances[msg.sender].transfer
-        return true;
+    //Get your funds back
+    function returnFunds(uint amount) external payable  {
+        require (balances[msg.sender] >= amount);
+        balances[msg.sender] -= amount;
+        contributedAmount -= amount;
+        (bool sent, ) = msg.sender.call{value: amount}("");
+        require(sent, "Fail transaction");
     }
 }
