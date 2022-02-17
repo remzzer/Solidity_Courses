46f4de31a8bef8e0c33eee333c975747e58392a3
remi <hellokiziche@gmail.com>
2022-02-17 14:29:08 +0100

    transfer_send

--- Soldity_Patterns_Cybersecurity/Transfer_Send.sol
+++ Soldity_Patterns_Cybersecurity/Transfer_Send.sol
@@ -4,15 +4,24 @@ pragma solidity >=0.7.0 <0.9.0;
 /*
 Instead of using "transfer" which reverts the code we can use "send" which returns a bool
 
-Write a function so only the owner can send Tatianna funds with the send method
+1. Write a function so only the owner can send Tatianna funds with the send method
 with logic to set up what happens if the amount is not sent.
+
+2. Write another function to claim refunds:
+Write a function called claimRefund which requires
+that the balance of msg.sender greater than 0 and transfer the balance to the current caller.
+Pseudo balance var named balance
 */
 
 contract A {
 
    address owner;
-    constructor (address _owner) {
 
+   //Retrieve amount of ETH by addresses
+   mapping(address => uint) public balance;
+
+    //Set owner (Here -> wallet that deploy contract)
+    constructor (address _owner) {
         owner = _owner;
     }
 
@@ -25,9 +34,18 @@ contract A {
         return owner;
     }
 
+    //Send ETH to Tatianna
     function sendTo(address payable _to) public payable onlyOwner {
+        balance[_to] += msg.value;
         bool sent = _to.send(msg.value);
         require(sent, "Failed to send Ether");
     }
 
+    //Tatianna can withdraw ETH
+    function claimRefund() external payable {
+        require(balance[msg.sender] > 0, "Not enough ETH");
+        balance[msg.sender] -= msg.value;
+        (bool sent, ) = msg.sender.call{value: balance[msg.sender]}("");
+        require(sent, "Failed to send Ether");
+    }
 }
