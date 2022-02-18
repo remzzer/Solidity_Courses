0176a7348b39fbf179b2726195066c2f16660ae9
remi <hellokiziche@gmail.com>
2022-02-18 18:10:47 +0100

    Events

---
+++ Advanced_Solidity/Events.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract LearnEvents {
+
+  // 1. Declare the event
+  // 2. emit the event
+
+  //only use 3 indexed per event
+  event NewTrade(
+      uint indexed date,
+      address from,
+      address indexed to,
+      uint indexed amount
+  );
+
+  function trade(address to, uint amount) external {
+      //outside consumer can see the event through web3js
+      //block.timestamp is a global varibale
+      emit NewTrade(block.timestamp, msg.sender, to, amount);
+
+  }
+}
