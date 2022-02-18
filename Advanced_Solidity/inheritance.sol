a0a2e81f79d1cb8472f26e147965862310f34dfa
remi <hellokiziche@gmail.com>
2022-02-18 17:46:29 +0100

    exam

--- Advanced_Solidity/inheritance.sol
+++ Advanced_Solidity/inheritance.sol
@@ -15,17 +15,25 @@ which will be executed one per contract and is invoked when a contract is create
   3. Contract B should inherit from contract A
   4. Contract B should have a function called outerAddTen which returns the calculation from innerAddTen function
   in Contract A to any given input
-  5. Create a function in contract B whch returns the value of innerVal from contract A.
+  5. Create a function in contract B which returns the value of innerVal from contract A.
 
 */
 
 
 contract A {
+  uint innerVal = 100;
 
-
+  function innerAddTen(uint n) public pure returns(uint){
+    return n + 10;
+  }
 }
 
-contract B {
+contract B is A {
+  function outerAddTen() public pure returns (uint){
+     return innerAddTen(1);
+  }
 
-  
+  function getInnerVal() public view returns (uint){
+      return innerVal;
+  }
 }
