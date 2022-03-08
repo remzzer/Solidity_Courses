ff5b12a7f0505666d89d297b18729eb61dbf7cd0
remi <hellokiziche@gmail.com>
2022-03-08 15:13:20 +0100

    uniswap

--- Advanced_Solidity_2/libraries.sol
+++ Advanced_Solidity_2/libraries.sol
@@ -2,19 +2,69 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 /*
-Libraries are similar to Contracts but are mainly intended for re use. A library contains functions which other contracts can call.
-Solidity have certain restrictions on use of a Library. Following are the key characteristics of a Solidity Library.
+A library contains functions which other contracts can call.
 
-    1. Pure and view functions can only be called from outside the library
-    2. Library can not be destroyed as it is assumed to be stateless.
-    3. A library cannot inherit any element.
-    4. A library cannot have state variables.
-    5. A library cannot be inherited.
+    1. Library can not be destroyed as it is assumed to be stateless.
+    2. A library cannot inherit any element.
+    3. A library cannot be inherited.
 
+Library
+    - no storage, no ether (state var / payable)
+    - helps you keep your code DRY (Don't Repeat Yourself)
+        - add functionality types
+        //uint x
+        //x.myFuncFromLibrary()
+    - Can save gas
+
+Embedded or linked
+    - embedded (library has only internal functions)
+        - When the contract will be deployed, all code from the library will be put inside the contract
+    - must be deployed and then linked (library has public or external functions)
 */
 
-library Search {
-    function indexOf(uint[] storage self, uint value) public view returns(uint){
+//Examples
+// - safe math (prevent uint overflow)
+// - deleting element from array without gaps
+
+library SafeMath {
+    function add(uint x, uint y) internal pure returns (uint){
+        uint z = x + y;
+        require(z >= x, "uint overflow");
+
+        return z;
+    }
+}
+
+contract TestSafeMath {
+    using SafeMath for uint;
+    //using A for B means
+    //attach functions from library A to type B
+
+    uint public MAX_UINT = 2 ** 256 - 1;
+
+    function testAdd(uint x, uint y) public pure returns (uint) {
+        return x.add(y); //First param is x
+        //return SafeMath.add(x, 456);
+    }
+}
+
+
+
+
+/*
+The directive using A for B can be used to attach library functions of library A to a given type B.
+These functions will use the caller type as their first parameter (identified using self).
+
+Exercice:
+1. Copy over the library Search and the contract Test below, rename library Search to Search2
+and contract Test to Test2.
+
+2. Using the A for B library pattern assign the new library to an empty array type and rewrite the code
+so that we can run the same search hardcoded this time to the value of 4.
+*/
+
+library Search2 {
+    function indexOf(uint[] storage self, uint value) public view returns(uint res){
         for(uint i=0; i < self.length; i++){
             if (self[i] == value){
                 return i;
@@ -23,7 +73,9 @@ library Search {
     }
 }
 
-contract Test {
+contract Test2 {
+    //import library
+    using Search2 for uint[];
     uint[] data;
     constructor() {
         data.push(1);
@@ -32,11 +84,12 @@ contract Test {
         data.push(4);
         data.push(5);
     }
-    function isValuePresent(uint val) external view returns(uint){
-        uint value = val;
 
-        //Call Search library
-        uint index = Search.indexOf(data, value);
+    function isValuePresent() external view returns(uint res){
+        uint value = 4;
+
+        //Use indexOf from Search2.
+        uint index = data.indexOf(value);
         return index;
     }
 }
